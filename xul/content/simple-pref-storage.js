cf96d4ccbef0ff5d4a2fc7df282f9b9ab736442e
JustOff <Off.Just.Off@gmail.com>
2018-03-02 19:25:33 +0200

    Add MPL-2.0 header to simple-pref-storage.js

--- xul/content/simple-pref-storage.js
+++ xul/content/simple-pref-storage.js
@@ -1,3 +1,8 @@
+/* Copyright (C) 2018 Off JustOff <Off.Just.Off@gmail.com>
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 Components.utils.import("resource://gre/modules/Services.jsm");
 
 var prefStorage = JSON.parse(Services.prefs.getCharPref("extensions.esrc-explorer.simpleStorage"));
