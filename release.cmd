72d6fd9b9432284180202c41aef1783289594cde
JustOff <Off.Just.Off@gmail.com>
2021-02-22 22:28:10 +0200

    Bump version

--- release.cmd
+++ release.cmd
@@ -1,5 +1,5 @@
 @echo off
-set VER=1.1.4
+set VER=1.1.5
 
 sed -i -b -E "s/version>.+?</version>%VER%</" xul\install.rdf
 sed -i -E "s/version>.+?</version>%VER%</; s/download\/.+?\/esrc-explorer-.+?\.xpi/download\/%VER%\/esrc-explorer-%VER%\.xpi/" update.xml
