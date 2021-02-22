d01966672a15fc781eecb44e41077d71ff60aaa3
JustOff <Off.Just.Off@gmail.com>
2021-02-22 15:50:59 +0200

    Set up release automation

---
+++ release.cmd
@@ -0,0 +1,11 @@
+@echo off
+set VER=1.1.4
+
+sed -i -b -E "s/version>.+?</version>%VER%</" xul\install.rdf
+sed -i -E "s/version>.+?</version>%VER%</; s/download\/.+?\/esrc-explorer-.+?\.xpi/download\/%VER%\/esrc-explorer-%VER%\.xpi/" update.xml
+
+call node make.js xul
+
+set XPI=esrc-explorer-%VER%.xpi
+if exist %XPI% del %XPI%
+copy dist\%XPI% .
